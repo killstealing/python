e1a024907288a4cb693555ea7758f5396e56954b
killstealing <13840935238@163.com>
2024-08-19 21:19:38 +0800

    add 10.4-10.5 code

---
+++ python_work/chapter10_fileAndException/cases10.12.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+import json
+
+def store_number():
+    file1=Path('python/python_work/chapter10_fileAndException/like_number1.txt')
+    number=input('pleaes enter your favorite number: ')
+    content=json.dumps(number)
+    file1.write_text(content)
+
+def get_number():
+    file1=Path('python/python_work/chapter10_fileAndException/like_number1.txt')
+    content=file1.read_text()
+    number=json.loads(content)
+    return number
+def greet_user():
+    file1=Path('python/python_work/chapter10_fileAndException/like_number1.txt')
+    if file1.exists():
+        number=get_number()
+    else:
+        store_number()
+        number=get_number()
+    print(f"I know your favorite number!It is {number}")
+
+greet_user()
+        
+        
+
