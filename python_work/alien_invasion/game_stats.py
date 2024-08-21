047fb9ea0287a3c08c175a2b2496ffcc72d6c715
killstealing <13840935238@163.com>
2024-08-21 23:41:09 +0800

    add chapter 14_score code

--- python_work/alien_invasion/game_stats.py
+++ python_work/alien_invasion/game_stats.py
@@ -4,6 +4,10 @@ class GameStats:
         """初始化统计信息"""
         self.settings=ai_game.settings
         self.reset_stats()
+        self.score=0
+        self.level=1
+        # 在任何情况下都不应该重置最高分
+        self.high_score=0
 
     def reset_stats(self):
         """初始化在游戏运行期间可能变化的统计信息"""
